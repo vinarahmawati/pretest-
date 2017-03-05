3a91b5719c4dc6ffd91ef8c4f83a8577e1960e8b
vinarahmawati <vinarahmawati43@gmail.com>
2017-03-05 17:41:53 +0700

    Create BiodataTugas8.java

---
+++ BiodataTugas8.java
@@ -0,0 +1,9 @@
+public class BiodataTugas8{
+	String nama = "Vina Rahmawati";
+	String alamat = "Pati";
+	int umur = 21;
+
+	void CetakBiodata(){
+		System.out.println("Nama : " + nama + "\nAlamat : " + alamat + "\nUmur : " + umur);
+	}
+}
